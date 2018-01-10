685ed973f43668bf160499b1ebf0b735d5826b2c
Hansong Zhang <hsz@google.com>
2018-01-10 13:43:25 -0800

    Fix unexpected behavior in reading BNEP packets
    Bug: 67863755
    Bug: 69177251
    Bug: 69177292
    Bug: 69271284
    Test: BNEP still works
    Change-Id: I41b8bfe5e123a56b8812124178663735f2bf3372
    (cherry picked from commit 9844ddac4c0aaf217326c56f2814d145c11eb042)
    CVE-2017-13258, CVE-2017-13260, CVE-2017-13261, CVE-2017-13262

--- stack/bnep/bnep_utils.c
+++ stack/bnep/bnep_utils.c
@@ -22,6 +22,8 @@
  *
  ******************************************************************************/
 
+#include <cutils/log.h>
+
 #include <stdio.h>
 #include <string.h>
 #include "gki.h"
@@ -835,6 +837,13 @@ UINT8 *bnep_process_control_packet (tBNEP_CONN *p_bcb, UINT8 *p, UINT16 *rem_len
         break;
 
     case BNEP_SETUP_CONNECTION_REQUEST_MSG:
+        if (*rem_len < 1) {
+          BNEP_TRACE_ERROR(
+              "%s: Received BNEP_SETUP_CONNECTION_REQUEST_MSG with bad length",
+              __func__);
+          android_errorWriteLog(0x534e4554, "69177292");
+          goto bad_packet_length;
+        }
         len = *p++;
         if (*rem_len < ((2 * len) + 1))
         {
@@ -864,6 +873,13 @@ UINT8 *bnep_process_control_packet (tBNEP_CONN *p_bcb, UINT8 *p, UINT16 *rem_len
         break;
 
     case BNEP_FILTER_NET_TYPE_SET_MSG:
+        if (*rem_len < 2) {
+          BNEP_TRACE_ERROR(
+              "%s: Received BNEP_FILTER_NET_TYPE_SET_MSG with bad length",
+              __func__);
+          android_errorWriteLog(0x534e4554, "69177292");
+          goto bad_packet_length;
+        }
         BE_STREAM_TO_UINT16 (len, p);
         if (*rem_len < (len + 2))
         {
@@ -891,6 +907,13 @@ UINT8 *bnep_process_control_packet (tBNEP_CONN *p_bcb, UINT8 *p, UINT16 *rem_len
         break;
 
     case BNEP_FILTER_MULTI_ADDR_SET_MSG:
+        if (*rem_len < 2) {
+          BNEP_TRACE_ERROR(
+              "%s: Received BNEP_FILTER_MULTI_ADDR_SET_MSG with bad length",
+              __func__);
+          android_errorWriteLog(0x534e4554, "69177292");
+          goto bad_packet_length;
+        }
         BE_STREAM_TO_UINT16 (len, p);
         if (*rem_len < (len + 2))
         {
