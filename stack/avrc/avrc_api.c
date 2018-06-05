bb87b88e5c4a2045cb63c1cfa462b760296e3096
Ajay Panicker <apanicke@google.com>
2018-06-05 16:08:06 -0700

    [Backport] DO NOT MERGE: Don't reuse buffer when building response
    Bug: 79541338
    Test: Compile and connect to remote headset
    Change-Id: I2d808f941d3c71fcb6306c733717624be10478e0
    (cherry picked from commit 9bbce8603846159dec0d506ba867b7616557a303)
    CVE-2018-9450
    
    Backport: #define BT_DEFAULT_BUFFER_SIZE

--- stack/avrc/avrc_api.c
+++ stack/avrc/avrc_api.c
@@ -32,6 +32,10 @@
 **  Global data
 *****************************************************************************/
 
+/* backport for CVE-2018-9450 */
+#ifndef BT_DEFAULT_BUFFER_SIZE
+#define BT_DEFAULT_BUFFER_SIZE          (4096 + 16)
+#endif
 
 #define AVRC_MAX_RCV_CTRL_EVT   AVCT_BROWSE_UNCONG_IND_EVT
 
@@ -321,15 +325,15 @@ static BT_HDR * avrc_proc_vendor_command(UINT8 handle, UINT8 label,
 
     if (status != AVRC_STS_NO_ERROR)
     {
-        /* use the current GKI buffer to build/send the reject message */
-        p_data = (UINT8 *)(p_pkt+1) + p_pkt->offset;
+        p_rsp = (BT_HDR *)GKI_getbuf(BT_DEFAULT_BUFFER_SIZE);
+        p_rsp->offset = p_pkt->offset;
+        p_data = (UINT8 *)(p_rsp + 1) + p_pkt->offset;
         *p_data++ = AVRC_RSP_REJ;
         p_data += AVRC_VENDOR_HDR_SIZE; /* pdu */
         *p_data++ = 0;                  /* pkt_type */
         UINT16_TO_BE_STREAM(p_data, 1); /* len */
         *p_data++ = status;             /* error code */
-        p_pkt->len = AVRC_VENDOR_HDR_SIZE + 5;
-        p_rsp = p_pkt;
+        p_rsp->len = AVRC_VENDOR_HDR_SIZE + 5;
     }
 
     return p_rsp;
@@ -483,7 +487,8 @@ static UINT8 avrc_proc_far_msg(UINT8 handle, UINT8 label, UINT8 cr, BT_HDR **pp_
             p_rsp = avrc_proc_vendor_command(handle, label, *pp_pkt, p_msg);
             if (p_rsp)
             {
-                AVCT_MsgReq( handle, label, AVCT_RSP, p_rsp);
+                AVCT_MsgReq(handle, label, AVCT_RSP, p_rsp);
+                GKI_freebuf(pp_pkt);
                 drop = 3;
             }
             else if (p_msg->hdr.opcode == AVRC_OP_DROP)
